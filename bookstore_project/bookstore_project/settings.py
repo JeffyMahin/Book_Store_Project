46719978fa8a31b434f86c13a26a91bd7f96d358
Mahin <jeffymahin@gmail..com>
2021-04-23 08:54:24 +0600

    Media config done for book cover

--- bookstore_project/bookstore_project/settings.py
+++ bookstore_project/bookstore_project/settings.py
@@ -183,3 +183,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 # LOGOUT_REDIRECT_URL = 'home'
 ACCOUNT_LOGOUT_REDIRECT_URL = 'home'
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
--- bookstore_project/bookstore_project/urls.py
+++ bookstore_project/bookstore_project/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf import settings  # media
+from django.conf.urls.static import static  # media
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,5 +9,5 @@ urlpatterns = [
     # path('accounts/', include('django.contrib.auth.urls')),
     # path('accounts/', include('users.urls')),
     path('accounts', include('allauth.urls')),
-    path('books/', include('books.urls'))
-]
+    path('books/', include('books.urls')),
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
